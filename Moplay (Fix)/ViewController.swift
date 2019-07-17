c19aaea0d27a7f181c0155d735b3c8ab3f882f78
Jason Valencius Wijaya <jasonwijaya097@gmail.com>
2019-07-17 15:37:07 +0700

    Initial Commit

---
+++ Moplay (Fix)/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Moplay (Fix)
+//
+//  Created by Jason Valencius Wijaya on 17/07/19.
+//  Copyright © 2019 Jason Valencius Wijaya. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
